78d1d8c5112f5a2c05badb02df63894129da518f
Alejandro Cruz <hcruz0716@gmail.com>
2022-11-01 20:13:30 -0600

    diferencia en meses de 2 fechas

--- motor.js
+++ motor.js
@@ -1,12 +1,26 @@
-function calculoMotor(typePayroll, Date, Gender) {
+function calculoMotor(typePayroll, day, Gender) {
   let tipoNomina = typePayroll;
-  let fechaPrimerEmpleo = Date;
+  let fechaPrimerEmpleo = day;
   let genero = Gender;
+  let today = new Date();
+  let meses = calcularDiferenciaMeses(new Date(fechaPrimerEmpleo), today);
 
-  console.log(tipoNomina, fechaPrimerEmpleo, genero);
+  console.log(tipoNomina, fechaPrimerEmpleo, genero, meses);
 
   // optimalLineOfCredit = max(p1, p2)
 
   // p1 = montoMinimo + / montoMaximo - montoMinimo
   // p2 = montominimo + 0.0175 * (montoMaximo - montoMinimo)
+}
+
+function calcularDiferenciaMeses(fecha1, fecha2) {
+  if (!(fecha1 instanceof Date) || !(fecha2 instanceof Date)) {
+    throw TypeError("Ambos argumentos deben ser objetos de tipo fecha (Date).");
+  }
+
+  let meses = (fecha2.getFullYear() - fecha1.getFullYear()) * 12;
+  meses -= fecha1.getMonth();
+  meses += fecha2.getMonth();
+
+  return meses - 1;
 }
