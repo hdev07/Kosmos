4e77a21c65c37664a212987d1e8aace3caa86d4a
Alejandro Cruz <hcruz0716@gmail.com>
2022-11-01 18:30:13 -0600

    first commit

---
+++ motor.js
@@ -0,0 +1,12 @@
+function calculoMotor(typePayroll, Date, Gender) {
+  let tipoNomina = typePayroll;
+  let fechaPrimerEmpleo = Date;
+  let genero = Gender;
+
+  console.log(tipoNomina, fechaPrimerEmpleo, genero);
+
+  // optimalLineOfCredit = max(p1, p2)
+
+  // p1 = montoMinimo + / montoMaximo - montoMinimo
+  // p2 = montominimo + 0.0175 * (montoMaximo - montoMinimo)
+}
